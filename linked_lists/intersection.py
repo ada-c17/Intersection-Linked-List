f52de3de67ed9c789275395dd59a04e76b1e5588
Katherine Otten Kim <kkatherine317@gmail.com>
2022-09-11 14:07:28 -0700

    complete implementations

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -1,14 +1,39 @@
-
-
+from multiprocessing.sharedctypes import Value
 
 class Node:
     def __init__(self, value):
         self.val = value
         self.next = None
 
-
 def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    
+    if not headA or not headB:
+        return None
+
+    stackA = []
+    stackB = []
+
+    pointerA = headA
+    pointerB = headB
+    intersection = None
+
+    while pointerA:
+        stackA.append(pointerA)
+        pointerA = pointerA.next
+    
+    while pointerB:
+        stackB.append(pointerB)
+        pointerB = pointerB.next
+    
+    while stackA and stackB:
+        currentNodeA = stackA.pop()
+        currentNodeB = stackB.pop()
+        if currentNodeA == currentNodeB:
+            intersection = currentNodeA
+        elif intersection and currentNodeA.val != currentNodeB.val:
+            return intersection
+    
+    return intersection
