7cbb289d28fd06611537245f4e2f4624ebcbe669
Lindsey M Still <lindseymstill@gmail.com>
2022-09-11 22:01:16 -0500

    passed tests

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -5,18 +5,6 @@ class Node:
     def __init__(self, value, next=None, previous=None):
         self.val = value
         self.next = next
-        self.previous = next
-
-def transform_to_doubly_linked_list(head):
-    previous = None
-    current = head
-    print(head)
-    while current is not None:
-        current.previous = previous
-        previous = current
-        current = current.next
-
-    return previous
 
 
 def intersection_node(headA, headB):
