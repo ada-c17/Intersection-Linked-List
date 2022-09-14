552416b19ad55e3fbaafae7993cd7bff3c70601b
soridalac <lacsorida@gmail.com>
2022-09-13 22:21:57 -0700

    add l1&l2 condition to intersection

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,8 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    l1, l2 = headA, headB
+    while l1 != l2:
+        l1 = l1.next if l1 else headB
+        l2 = l2.next if l2 else headA
+    return l1
