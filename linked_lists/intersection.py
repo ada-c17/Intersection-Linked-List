f199b7ebf7e2e45e1e3187295dd846942c76cf07
Hillary Smith <hillaryhsmith@gmail.com>
2022-09-14 18:58:37 -0700

    make function to find intersection node

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,22 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    currentA = headA
+    currentB = headB 
+
+    # nested while loops
+    while currentA is not None:
+        while currentB is not None: 
+            if currentA is currentB: 
+                return currentA
+
+            currentB = currentB.next
+        
+        currentA = currentA.next        
+        currentB = headB
+    
+    return None
+
+
+
+
