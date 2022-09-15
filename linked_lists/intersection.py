cc1984fb6c3e9ef179b1ab8da08d320d106fa605
Doina Colun <doinacolun@macpro-2.local>
2022-09-15 00:05:54 -0700

    Intersection of Linked Lists

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -1,14 +1,23 @@
-
-
-
 class Node:
     def __init__(self, value):
         self.val = value
         self.next = None
 
-
+   
 def intersection_node(headA, headB):
-    """ Will return the node at which the two lists intersect.
-        If the two linked lists have no intersection at all, return None.
-    """
-    pass
+    nodes = set()
+    
+    it = headA
+    while not it is None:
+        nodes.add(it)
+        it = it.next
+    
+    it = headB
+    while not it is None:
+        if it in nodes:
+            return it
+        it = it.next
+    
+    return None
+
+
