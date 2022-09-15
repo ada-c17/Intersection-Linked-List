106fed566b2d66b5af2531c7f41b17a8d9dd4f54
char <charmara.mays@adadevelopersacademy.org>
2022-09-15 11:55:18 -0700

    add solution

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -10,5 +10,11 @@ class Node:
 def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
+        Time Complexity: O(n+m)
+        Space Complexity: O(1)
     """
-    pass
+    l1, l2 = headA, headB
+    while l1 != l2:
+        l1 = l1.next if l1 else headB
+        l2 = l2.next if l2 else headA
+    return l1
