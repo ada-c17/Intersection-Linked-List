1f9f897af66fe45e24852b89ed4190ee8642dbd1
Sophia Wagner <sophiewagnerla@gmail.com>
2022-09-15 18:08:04 -0700

    Implements interesction_node function to pass all 6 tests

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,14 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    current_A = headA
+    while current_A is not None:
+        current_B = headB
+        while current_B is not None:
+            if current_A == current_B:
+                return current_A
+            else:
+                current_B = current_B.next
+        current_A = current_A.next
+
+    return None
