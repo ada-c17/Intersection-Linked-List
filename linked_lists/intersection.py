93da8d191a87a9c0488cf5bab2b92dab51c2b13c
Jenny C <jennyhc888@gmail.com>
2022-09-17 00:18:28 -0700

    finish problem

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -10,5 +10,24 @@ class Node:
 def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
+
     """
-    pass
+    hashSet = set ()
+    hA, hB = headA, headB
+
+
+    if not hA or not hB:
+        return None
+
+
+    while hA: 
+        hashSet.add(hA) 
+        hA = hA.next
+
+    while hB:
+        if hB in hashSet:
+            return hB
+        hB = hB.next
+    return None
+
+
