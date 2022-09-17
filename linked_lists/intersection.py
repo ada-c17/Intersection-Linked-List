ae70a561f370fe4ba9fa01ca3f0c3aa54b8d5186
Gabriela Webb <gabw813@gmail.com>
2022-09-17 17:21:40 -0400

    passes same length list test case

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -13,3 +13,46 @@ def intersection_node(headA, headB):
     """
     if not headA or not headB:
         return None
+    
+    currentA = headA
+    currentB = headB
+
+    while currentA.next and currentB.next:
+        print(currentA.val, currentB.val)
+        if currentA == currentB:
+            return currentA
+        currentA = currentA.next
+        currentB = currentB.next
+        # print(currentA.val, currentB.val)
+    return None
+        
+
+# node_d = Node("D")
+# node_e = Node("E")
+# node_f = Node("F")
+
+# node_x = Node("X")
+# node_y = Node("Y")
+# node_z = Node("Z")
+
+# node_one = Node("1")
+# node_two = Node("2")
+# node_three = Node("3")
+# node_one.next = node_two
+# node_two.next = node_three
+
+# # List A: ["D", "E", "F", "1", "2", "3"]
+# node_d.next = node_e
+# node_e.next = node_f
+# node_f.next = node_one
+
+# # List B: ["X", "Y", "Z", "1", "2", "3"]
+# node_x.next = node_y
+# node_y.next = node_z
+# node_z.next = node_one
+
+# head_a = node_d
+# head_b = node_x
+
+# # Act
+# print(intersection_node(head_a, head_b))
