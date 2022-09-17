426013a6e8b6ea5a5bd9a76651ec0a254ad15413
Danielle <dmbecker24@gmail.com>
2022-09-17 16:06:54 -0700

    pass all tests

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -8,7 +8,26 @@ class Node:
 
 
 def intersection_node(headA, headB):
-    """ Will return the node at which the two lists intersect.
-        If the two linked lists have no intersection at all, return None.
-    """
-    pass
+
+    current_a = headA
+    current_b = headB
+    hash_map = {}
+
+    if not current_a or not current_b:
+        return None
+
+    while current_a:
+        hash_map[current_a] = 1
+        current_a = current_a.next
+
+    while current_b:
+        if current_b in hash_map.keys():
+            return current_b
+        current_b = current_b.next
+    
+    return None
+
+    
+
+
+
