75e2e01a5e2b37a87d3b8e66d0bbd9fb27573351
Anna Pranovich <programming.aap@gmail.com>
2022-09-16 18:37:28 -1000

    Tests pass

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -10,6 +10,9 @@ class Node:
 def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
+
+        Iterate through 1st linked list and add its nodes to set.
+        Iterate through 2nd linked list: if node is in set -> return node; otherwise return None
     """
 
     currA, currB, valA = headA, headB, set()
