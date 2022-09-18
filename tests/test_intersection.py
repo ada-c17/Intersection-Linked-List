9c20a7fa6fc4f8ca116fdeab98836e8ac76cad75
Jenny <jfperham@gmail.com>
2022-09-18 11:12:00 -0700

    Add one test to check for when 2nd linked list is longer

--- tests/test_intersection.py
+++ tests/test_intersection.py
@@ -37,6 +37,7 @@ def test_will_return_intersection_for_lists_of_same_length():
     # Assert
     assert answer == node_one
 
+
 def test_will_return_intersection_with_lists_of_differing_lengths():
     # Arrange
     node_d = Node("D")
@@ -68,6 +69,41 @@ def test_will_return_intersection_with_lists_of_differing_lengths():
     # Assert
     assert answer == node_one
 
+# Add test for listB longer than listA:
+
+
+def test_will_return_intersection_with_lists_of_differing_lengths_second_list_longer():
+    # Arrange
+    node_d = Node("D")
+    node_e = Node("E")
+    node_f = Node("F")
+
+    node_x = Node("X")
+
+    node_one = Node("1")
+    node_two = Node("2")
+    node_three = Node("3")
+    node_one.next = node_two
+    node_two.next = node_three
+
+    # List A: ["D", "E", "F", "1", "2", "3"]
+    node_d.next = node_e
+    node_e.next = node_f
+    node_f.next = node_one
+
+    # List B: ["X", "1", "2", "3"]
+    node_x.next = node_one
+
+    head_a = node_d
+    head_b = node_x
+
+    # Act
+    answer = intersection_node(head_b, head_a)
+
+    # Assert
+    assert answer == node_one
+
+
 def test_will_return_none_with_one_empty_list():
     # Arrange
     node_d = Node("D")
@@ -85,6 +121,7 @@ def test_will_return_none_with_one_empty_list():
 
     # Assert
     assert answer is None
+
 
 def test_will_return_none_when_no_intersection():
     # Arrange
@@ -113,6 +150,7 @@ def test_will_return_none_when_no_intersection():
     # Assert
     assert answer is None
 
+
 def test_will_return_none_for_two_empty_lists():
     # Arrange
 
@@ -127,7 +165,7 @@ def test_will_return_none_for_two_empty_lists():
 
 
 def test_will_return_none_nodes_have_same_value_different_reference():
-    #Arrange
+    # Arrange
     node_d1 = Node("D")
     node_e1 = Node("E")
     node_f1 = Node("F")
@@ -136,19 +174,19 @@ def test_will_return_none_nodes_have_same_value_different_reference():
     node_e2 = Node("E")
     node_f2 = Node("F")
 
-    #List A: [d, e1, f1]
+    # List A: [d, e1, f1]
     node_d1.next = node_e1
     node_e1.next = node_f1
 
-    #List B: [c, e2, f2]
+    # List B: [c, e2, f2]
     node_c1.next = node_e2
     node_e2.next = node_f2
 
     head_a = node_d1
     head_b = node_c1
 
-    #Act
+    # Act
     answer = intersection_node(head_a, head_b)
 
-    #Assert
+    # Assert
     assert answer is None
