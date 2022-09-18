628f8f69bb0a05a2292e6d433b2241514f76c1e6
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-09-18 16:15:12 -0700

    Passing 5/6 tests

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,20 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    
+    if not headA or not headB:
+        return None
+    
+    currentA = headA
+    currentB = headB
+    intersect = None
+
+    while currentA:
+    
+        if currentA == currentB:
+            intersect = currentA
+        else:
+            currentB = currentB.next
+        currentA = currentA.next
+        
+    return intersect
