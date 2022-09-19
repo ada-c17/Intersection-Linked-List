6856feaf020e333c7fd7286b9d08435bb7fbad03
Nishat <nsalsabil25@gmail.com>
2022-09-19 11:55:58 -0700

    switched order of headA and headB, all testing passing

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -13,6 +13,6 @@ def intersection_node(headA, headB):
     """
     list1, list2 = headA, headB
     while list1 != list2:
-        list1 = list1.next if list1 else headB
-        list2 = list2.next if list2 else headA
+        list1 = list1.next if list1 else headA
+        list2 = list2.next if list2 else headB
     return list1
