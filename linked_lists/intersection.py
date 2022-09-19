8788c47dfc8fd158a8fd71e15f8245ee760e53d5
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-09-18 21:20:08 -0500

    Complete intersection_node function

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -1,6 +1,3 @@
-
-
-
 class Node:
     def __init__(self, value):
         self.val = value
@@ -11,4 +8,19 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    if not headA or not headB:
+        return None
+    
+    nodes_in_LL_A = set()
+    curr_A = headA
+    while curr_A:
+        nodes_in_LL_A.add(curr_A)
+        curr_A = curr_A.next
+
+    curr_B = headB
+    while curr_B:
+        if curr_B in nodes_in_LL_A:
+            return curr_B
+        curr_B = curr_B.next
+    
+    return None
