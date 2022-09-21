ecb50301c5b9d6ba782ec1e8a42c28af51ac023f
Myranae <myranae@pm.me>
2022-09-20 23:03:03 -0400

    passes all tests!

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -6,17 +6,6 @@ class Node:
         self.val = value
         self.next = None
 
-    # def search(self, value):
-    #     current = self.head
-
-    #     while current:
-    #         if current.val == value:
-    #             return True
-    #         current = current.next
-        
-    #     return False
-
-
 def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
@@ -28,34 +17,59 @@ def intersection_node(headA, headB):
     while current != None:
         list_a.append(current.val)
         current = current.next
+    print(f"lista: {list_a}")
 
     current = headB
     while current != None:
         list_b.append(current.val)
         current = current.next
+    print(f"listb: {list_b}")
+
 
     if not list_a or not list_b:
         return None
 
-    # intersect_node_index = None
-    # for i in range(len(list_a)):
-    #     if list_a[i] == list_b[i]:
-    #         intersect_node_index = i
-    #         if list_a[i:] != list_b[i:]:
-    #             return None
-
     # List A: ["D", "E", "F", "1", "2", "3"]
     # List B: ["X", "1", "2", "3"]
+
+    # intersect_value = None
+
+    intersection_list = []
+
+
+    # a = 0
+    # b = 0
+
+    # while list_a[a] and list_b[b]:
+    #     if
+    
+    # while list_a[a] and list_b[b]:
+    #     if list_a[a] == list_b[b]:
+    #         intersection_list.append(list_a[a])
+    #         a += 1
+    #         b += 1
+    #     elif list_a[a] ==
+    i=-1
+    while list_a[i] == list_b[i] and abs(i)<len(list_a):
+        intersection_list.append(list_a[i])
+        i -= 1
+
+    if intersection_list:
+        intersection_list.reverse()
+        print(f"intersection_list: {intersection_list}")
+    else:
+        return None
+    
 
 
     # need to find the intersection without using a list as I need 
     # the intersection values to be in order I think
-    set_a = set(list_a)
-    set_b = set(list_b)
-    intersection = set_a.intersection(set_b)
+    # set_a = set(list_a)
+    # set_b = set(list_b)
+    # intersection = set_a.intersection(set_b)
 
-    if not intersection:
-        return None
+    # if not intersection_list:
+    #     return None
 
     # current = headA
     # while current:
@@ -69,7 +83,7 @@ def intersection_node(headA, headB):
     intersect = None
 
     while currentA and currentB:
-        for item in intersection:
+        for item in intersection_list:
             if currentA.val == item and currentB.val == item:
                 if currentA == currentB:
                     if intersect == None:
