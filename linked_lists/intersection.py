f9fffc26f997d27052ec9f0b933c358cadd54fc8
MandyG415 <missmandygao@gmail.com>
2022-09-21 21:23:12 -0700

    Passed 4 out of 6 tests, need to find logic for lists with different lengths

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,8 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    while headA != headB:
+      headA = headA.next
+      headB = headB.next
+
+    return headB
