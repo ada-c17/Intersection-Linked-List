f88d8ecc33d57c6e7aa54f1ee6baee36cfacc760
Megan Korling <korlingm@gmail.com>
2022-09-22 15:06:38 -0700

    updated solution with better time complexity

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -12,34 +12,20 @@ def intersection_node(headA, headB):
         Lists are not necessarily the same length.
         If the two linked lists have no intersection at all, return None.
     """
-    # brute force solution?
-    intersection_at = None
-    currentA = headA
-    # traverse through A
-    while currentA:
-        currentB = headB
-        # traverse through B
-        while currentB:
-            # if they match:
-            if currentA == currentB:
-                # traverse through remainder of A and B at same time
-                # make new pointer variables to not lose place of currentA and currentB
-                inner_currentA = currentA
-                inner_currentB = currentB
-                while inner_currentA and inner_currentB:
-                    if inner_currentA == inner_currentB and intersection_at is None:
-                        intersection_at = inner_currentA
-                        
-                    elif inner_currentA != inner_currentB:
-                        intersection_at = None
-                        break
-                    # subsequent nodes match, increment to next node
-                    inner_currentA = inner_currentA.next
-                    inner_currentB = inner_currentB.next
-                if intersection_at:
-                    return intersection_at
-            currentB = currentB.next
-        currentA = currentA.next
-    return intersection_at
+    if not headA or not headB:
+        return None
+
+    ha = headA
+    hb = headB
+
+    while ha is not hb:
+        # Move node of each list to next one
+        # If it reaches the end and no match is found, point it to start of other linked list
+        ha = headB if ha is None else ha.next
+        hb = headA if hb is None else hb.next
+
+    # If they instersect, either ha or hb point to the same node
+    # If they don't intersect, ha or hb are at the end of the list and are None
+    return ha
 
 
