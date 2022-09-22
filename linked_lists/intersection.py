27a3147a0db1ee990e3e4ca1775e621a5add3138
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-09-22 11:43:14 -0500

    Update solution to account for lists of different lengths; pass all tests

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -10,50 +10,29 @@ class Node:
 def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
-
-        - set var to track whether lists intersect equal to false
-        - set both current nodes equal to head nodes
-        - matching nodes = []
-    
-        - while current node exists for both lists:
-        - if both current nodes are equal to each other, append current node value to matching nodes array
-        - and set do_intersect equal to true
-        - set both current nodes to next nodes
-
-        - if do_intersect: return matching_nodes[0] else return None
     """
-    # do_intersect = False
-    # current_node_a = headA
-    # current_node_b = headB
+    if not headA:
+        return None
 
-    # matching_values = []
+    current_node_a = headA
+    current_node_b = headB
 
-    # while current_node_a.val and current_node_b.val:
-    #     if current_node_a.val == current_node_b.val:
-    #         matching_values.append(current_node_a.val)
-    #         do_intersect = True
-    #     else:
-    #         do_intersect = False
-        
-    #     current_node_a = current_node_a.next
-    #     current_node_b = current_node_b.next
-    
-    # return matching_values[0] if do_intersect else None
+    list_b_map = {}
+    intersection_point = None
 
-    do_intersect = False
-    current_node_a = headA.next
-    current_node_b = headB.next
-
-    matching_values = []
-
-    while current_node_a.val and current_node_b.val:
-        if current_node_a.val == current_node_b.val:
-            matching_values.append(current_node_a.val)
-            do_intersect = True
-        else:
-            do_intersect = False
-        
-        current_node_a = current_node_a.next
+    while current_node_b:
+        list_b_map[current_node_b] = current_node_b.next
         current_node_b = current_node_b.next
+
+    current_node_b = headB
+
+    while current_node_a and current_node_b:
+        if current_node_a in list_b_map and not intersection_point:
+            intersection_point = current_node_a
+            current_node_b = list_b_map[current_node_a]
+        elif current_node_a not in list_b_map:
+            intersection_point = None
     
-    return Node(matching_values[0]) if do_intersect else None
+        current_node_a = current_node_a.next
+    
+    return intersection_point
