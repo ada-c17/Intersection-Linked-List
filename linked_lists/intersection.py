4dff6b28a079bdb327eb0982de086e436ef652a1
sarah <skiyoko14@gmail.com>
2022-09-21 21:27:06 -0700

    solution with time complexity a*b passes all tests

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,18 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    if not headA or not headB:
+        return None
+
+    a_node = headA
+    b_node = headB
+
+    while a_node:
+        while b_node:
+            if a_node == b_node:
+                return a_node
+            b_node = b_node.next
+        a_node = a_node.next
+        b_node = headB
+    
+    return None
