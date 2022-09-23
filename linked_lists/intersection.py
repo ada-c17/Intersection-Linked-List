26cbf4e14928fa60449b81e99e2c4d67d14431a4
MandyG415 <missmandygao@gmail.com>
2022-09-22 18:24:11 -0700

    Passed all tests, including lists with diff lengths

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,8 +11,20 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    while headA != headB:
-      headA = headA.next
-      headB = headB.next
+    if headB == None or headA == None:
+      return None
 
-    return headB
+    list_of_nodes = []
+    current = headB
+    while current != None:
+      list_of_nodes.append(current)
+      current = current.next
+    
+    currentA = headA
+    while currentA != None:
+      if currentA in list_of_nodes:
+        return currentA
+      else:
+        currentA = currentA.next
+    #loop through listA unknown nodes to see if present in listB. 
+    return None
