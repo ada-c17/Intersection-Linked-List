5ece12a77b298cbc70b61ecf46654d835d56cbf9
Vida Maleki <vida.basmani@gmail.com>
2022-09-25 21:35:07 -0700

    finished intersection project

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,17 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    if not headA or not headB:
+        return None
+    
+    current_item_a = headA
+    
+    while current_item_a:
+        current_item_b = headB
+        while current_item_b:
+            if current_item_a == current_item_b:
+                return current_item_a
+            current_item_b = current_item_b.next
+        current_item_a = current_item_a.next
+    
+    return None
