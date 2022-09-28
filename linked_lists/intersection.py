bf7a9931ca26beb0339a12befe65f3388f214142
Natascha <njenkns@gmail.com>
2022-09-28 15:58:46 -0700

    solution passes all tests

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -10,5 +10,13 @@ class Node:
 def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
+
+        Full disclaimer: I've done this leetcode question before with my tutor and so I had this solution - that's the only reason it's so elengant, lol. I initially
+        tried it with a hash set, but this solution has better space complexity
     """
-    pass
+    node_1, node_2 = headA, headB
+
+    while node_1 != node_2:
+        node_1 = node_1.next if node_1 else headB
+        node_2 = node_2.next if node_2 else headA
+    return node_2
