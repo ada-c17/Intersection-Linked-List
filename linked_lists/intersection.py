17745aeccc010aaac9dc1810aaa9f58a8d21432b
Kelly Kang <shkx281@gmail.com>
2022-09-28 23:30:13 -0700

    Passed all tests

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,20 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    currA = headA
+    currB = headB
+
+    A_nodes = set()
+
+    while currA:
+        A_nodes.add(currA)
+        currA = currA.next
+
+    
+    while currB:
+        if currB in A_nodes:
+            return currB
+        currB = currB.next
+    
+
+    return None
