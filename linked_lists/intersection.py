b5c6daa8499e0544dc9394fe03e68c03e1370cf2
exitexit <eunicegore@gmail.com>
2022-09-28 21:00:08 -0700

    passing tests

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,17 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    currentA =  headA
+    currentB = headB
+    
+    while currentA != currentB:
+        if currentA is not None:
+            currentA = currentA.next
+        else:
+            currentA = headB
+        if currentB is not None:
+            currentB = currentB.next
+        else:
+            currentB = headA
+
+    return currentA
