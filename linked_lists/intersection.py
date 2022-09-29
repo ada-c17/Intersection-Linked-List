9eee803cc0b107d19e5ac223e59bb273dd662733
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-09-29 11:00:56 -0700

    linked lists first implementation

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -6,9 +6,24 @@ class Node:
         self.val = value
         self.next = None
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
 def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    if headA is None or headB is None:
+        return None
+    
+    currentA = headA 
+    currentB = headB
+    while currentA is not None:
+        while currentB is not None:
+            if currentA.val == currentB.val:
+                return currentA
+            currentB = currentB.next
+        currentA = currentA.next
+        currentB = headB
+    return None
