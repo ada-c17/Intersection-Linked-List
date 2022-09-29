327779b4b0e455c392adf5ae162fb53cae3a6ae0
Grace <gpark439@gmail.com>
2022-09-29 13:15:40 -0700

    Return node at which two lists intersect

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,16 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    visited_nodes = set()
+
+    while headA:
+        visited_nodes.add(headA)
+        headA = headA.next
+
+    while headB:
+        if headB in visited_nodes:
+            return headB
+        headB = headB.next
+
+    return None
+
