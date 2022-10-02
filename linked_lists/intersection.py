13610f74938454ecec87d13ddff0987d8dffae8d
Taingleap Chheang <chheangtaingleap@gmail.com>
2022-10-01 22:26:51 -0700

    passed all tests

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -1,6 +1,4 @@
 
-
-
 class Node:
     def __init__(self, value):
         self.val = value
@@ -11,4 +9,19 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    if not headA or not headB:
+        return None
+
+    setA = set()
+
+    currentA, currentB = headA, headB
+    while currentA:
+        setA.add(currentA)
+        currentA = currentA.next
+
+    while currentB:
+        if currentB in setA:
+            return currentB
+        currentB = currentB.next
+    
+    return None
