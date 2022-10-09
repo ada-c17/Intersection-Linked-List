0f34b3c4c78d56cff8c17b9df534359c8fbd536e
Tirhas Gebreyohannes <tirhastse@gmail.com>
2022-10-08 20:08:07 -0700

    All tests passed

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -6,9 +6,28 @@ class Node:
         self.val = value
         self.next = None
 
-
+#  time complexity O(n)
+#  space complexity O(n)
 def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    if headA is None or headB is None:
+        return None
+    elif headA is None and headB is None:
+        return None
+    else:
+        current = headA
+        nodes_set = set()
+        while current:
+            if current not in nodes_set:
+                nodes_set.add(current)
+            current = current.next
+            
+        current_b =  headB   
+        while current_b:
+            if current_b in  nodes_set:
+                return current_b
+            current_b = current_b.next   
+        return None
+        
