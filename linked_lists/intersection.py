59cb4b9c8fca6588097481fc14a8585d051e4441
syblades <86990570+syblades@users.noreply.github.com>
2022-11-01 14:56:49 -0700

    All tests passing

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,17 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    head_one = headA
+    head_two = headB
+    while head_one != head_two:
+        if head_one:
+            head_one = head_one.next
+        else:
+            head_one = headB
+        if head_two:
+            head_two = head_two.next
+        else:
+            head_two = headA
+    return head_one
+
+
