33fd6f0b089101d6430169e718918a7522a35093
San <san.robinxin@gmail.com>
2022-12-30 13:24:16 -0500

    implemented node function

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,21 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    pointer_a = headA
+    pointer_b = headB
+
+    while pointer_a and pointer_b:
+        if pointer_a == pointer_b:
+            return pointer_a
+
+        pointer_a = pointer_a.next 
+        pointer_b = pointer_b.next 
+
+        if pointer_a == pointer_b:
+            return pointer_a
+        if not pointer_a:
+            pointer_a = headB
+        if not pointer_b:
+            pointer_b = headA    
+    
+    return None
