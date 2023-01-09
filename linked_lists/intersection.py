4eeb89554360457ce033ae1c8f3c759a81f7d766
Poppy Meng <qim17@pitt.edu>
2023-01-08 21:27:09 -0800

    all tests passed

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,11 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    if not headA or not headB:
+        return None
+    p1, p2 = headA, headB
+    while p1.val!= p2.val:
+        p1 = p1.next if p1.next else headB
+        p2 = p2.next if p2.next else headA
+
+    return p1
