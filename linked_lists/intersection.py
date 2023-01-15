b00d2ff290e396517dc1694e2b3bfa8c775ae213
Ada <adabarries@gmail.com>
2023-01-14 22:35:33 -0800

    resolved.

--- linked_lists/intersection.py
+++ linked_lists/intersection.py
@@ -11,4 +11,17 @@ def intersection_node(headA, headB):
     """ Will return the node at which the two lists intersect.
         If the two linked lists have no intersection at all, return None.
     """
-    pass
+    current_A = headA
+    current_B = headB
+
+    while current_A:
+        while current_B:
+            if current_A == current_B:
+                return current_A
+            else:
+                current_B = current_B.next
+        current_A = current_A.next
+        current_B = headB        
+
+
+    return None
